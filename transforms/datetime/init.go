38ebefa867669bba46cc2c9e1d8a231433059395
Daniel Kumor <daniel@dkumor.com>
2016-01-08 20:05:12 -0600

    Weekday
    Needs more tests, but this is the basic code...

---
+++ transforms/datetime/init.go
@@ -0,0 +1,31 @@
+/*Package datetime offers transforms useful for processing timestamps*/
+package datetime
+
+import (
+	"time"
+
+	"github.com/connectordb/pipescript"
+)
+
+var timezoneArg = pipescript.TransformArg{
+	Description: "The time zone to use for determining timestamps, in IANA timezone database format (ex: 'America/New_York'). 'Local' uses the server time zone. 'UTC' uses UTC.",
+	Optional:    true,
+	Default:     "Local",
+	Constant:    true,
+}
+
+func getTimezone(arg *pipescript.Script) (*time.Location, error) {
+	dp, err := arg.GetConstant()
+	if err != nil {
+		return nil, err
+	}
+	v, err := dp.String()
+	if err != nil {
+		return nil, err
+	}
+	return time.LoadLocation(v)
+}
+
+func init() {
+	weekday.Register()
+}
---
+++ transforms/datetime/weekday_test.go
@@ -0,0 +1,24 @@
+package datetime
+
+import (
+	"testing"
+	"time"
+
+	"github.com/connectordb/pipescript"
+)
+
+func TestWeekday(t *testing.T) {
+	pipescript.TestCase{
+		Pipescript: "weekday('lol')",
+		ParseError: true,
+	}.Run(t)
+	pipescript.TestCase{
+		Pipescript: "weekday",
+		Input: []pipescript.Datapoint{
+			{float64(time.Now().Unix()), 1},
+		},
+		Output: []pipescript.Datapoint{
+			{float64(time.Now().Unix()), time.Now().Weekday().String()},
+		},
+	}.Run(t)
+}
