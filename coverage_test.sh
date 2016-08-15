a20e6d660e0e900f17e358aef61e664cfc91d9e4
Daniel R. Kumor <daniel@dkumor.com>
2016-08-15 02:31:32 -0400

    Avoid coveralls 504 errors
    goveralls includes shallow option not to explode when these happen

--- coverage_test.sh
+++ coverage_test.sh
@@ -24,4 +24,4 @@ done
 
 # To submit the test coverage result to coveralls.io,
 # use goveralls (https://github.com/mattn/goveralls)
-goveralls -coverprofile=profile.cov -service=travis-ci
+goveralls -coverprofile=profile.cov -service=travis-ci -shallow
